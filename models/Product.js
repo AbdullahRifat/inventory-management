f60883d81d85b2de5c29bb954ccc7ac9e937ad0e
AbdullahRifat <abdrifat69@gmail.com>
2025-07-23 18:52:02 +0600

    added

--- models/Product.js
+++ models/Product.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  name: String,
-  quantity: Number,
-  price: Number
+  name: { type: String, required: true },
+  quantity: { type: Number, required: true },
+  price: { type: Number, required: true,default: 0 },
 });
 
 module.exports = mongoose.model('Product', productSchema);
